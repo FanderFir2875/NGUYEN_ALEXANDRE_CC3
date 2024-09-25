342aec864e0dc4e5106d2ba2cf8fed54cab18480
FanderFir2875 <nguyenalexandre700@gmail.com>
2024-09-25 20:43:09 +1100

    COMMIT 1
    Question 1.1

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
+
 # NGUYEN_ALEXANDRE_CC3
- CC3 TP5
+
+## Question 1.1
+
+Dans cette question, nous avons une fonction `requestListener` qui génère une page HTML simple avec le message **"My first server!"**.  
+
+### En-têtes renvoyés :
+```json
+Headers in JSON format: {
+  "connection": "keep-alive",
+  "date": "Wed, 25 Sep 2024 09:38:17 GMT",
+  "keep-alive": "timeout=5",
+  "transfer-encoding": "chunked"
+}
+```
+
+Quand cette page est affichée dans un navigateur, le contenu HTML est bien affiché avec "My first server".
+
+---
---
+++ entete.js
@@ -0,0 +1,26 @@
+
+// URL de votre serveur
+const url = 'http://localhost:8000';
+
+// Fonction pour récupérer et afficher les en-têtes
+async function fetchHeaders() {
+    try {
+        // Effectuer la requête
+        const response = await fetch(url);
+        
+        // Créer un objet JSON pour stocker les en-têtes
+        const headersJson = {};
+        response.headers.forEach((value, key) => {
+            headersJson[key] = value;
+        });
+
+        // Afficher les en-têtes au format JSON
+        console.log('Headers in JSON format:', JSON.stringify(headersJson, null, 2));
+    } catch (error) {
+        console.error('Error fetching headers:', error);
+    }
+}
+
+// Appeler la fonction
+fetchHeaders();
+
